68aa2d8ca3d5225429f2a6a01c89f93ea772f64f
marzie <marzie.7900@gmail.com>
2021-04-23 15:27:18 +0430

    add email password

--- sky_movex/settings/base.py
+++ sky_movex/settings/base.py
@@ -16,6 +16,9 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+EMAIL_HOST_USER = "skymovextest@gmail.com"
+EMAIL_HOST_PASSWORD = "iwaihdilbqyntqdv"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
