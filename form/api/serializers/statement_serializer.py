698078bbdbbd7c9443d41179e5579513d7449f73
marzie <marzie.7900@gmail.com>
2021-06-17 20:14:33 +0430

    repair a blance formule

--- form/api/serializers/statement_serializer.py
+++ form/api/serializers/statement_serializer.py
@@ -12,9 +12,11 @@ class StatemnetCreateSerializer(serializers.ModelSerializer):
         max_order = Statement.objects.aggregate(Max('order'))
         if max_order:
             max_value = max_order["order__max"]
+            max_obj = Statement.objects.filter(order=max_value)
+            balance = validated_data["charge"] - max_obj.balance
         else:
             max_value = 0
         statement = Statement.objects.create(order=max_value, charge=validated_data["charge"]
         , date=validated_data["date"], consignee=validated_data["consignee"]
-        , currency=validated_data["currency"])
+        , currency=validated_data["currency"], balance=balance)
         return statement
